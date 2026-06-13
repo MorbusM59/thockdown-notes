ef077d5ddcf9941cb5b19f9923f782d691f1dfa1
Morbus <fonkykitten@gmail.com>
2026-06-13 09:03:21 +0200

    b038

--- electron/main.ts
+++ electron/main.ts
@@ -420,7 +420,6 @@ async function createWindow() {
   // Test active push message to Renderer-process.
   win.webContents.on('did-finish-load', () => {
     win?.webContents.send('main-process-message', (new Date).toLocaleString())
-    flushPendingExternalPathsToRenderer()
   })
 
   if (VITE_DEV_SERVER_URL) {
